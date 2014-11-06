9e4713af07858a733e6f6f15bf4f559d2acc2d3f
SnakerWorker <snakerworker@gmail.com>
2014-11-06 19:19:42 +0800

    modify getPromotionTypes method

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDaoImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDaoImpl.java
@@ -1,10 +1,12 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class PromotionDaoImpl extends DbUtils implements PromotionDao {
@@ -13,21 +15,28 @@ public class PromotionDaoImpl extends DbUtils implements PromotionDao {
     private PreparedStatement preparedStatement;
     private ResultSet resultSet;
 
+    private JdbcTemplate jdbcTemplate ;
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public List<Integer> getPromotionTypes(int id) throws SQLException {
 
-        List<Integer> promotionTypes = new ArrayList<Integer>();
+        List<Integer> promotionTypes;
         String sql = "select p2.type from productPromotions p1 ,promotion p2 where p2.id = p1.promotion_id and p1.product_id=?";
 
-        connection = getConnection();
-        preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setInt(1,id);
-        resultSet = preparedStatement.executeQuery();
-
-        while (resultSet.next()){
-            int type = resultSet.getInt("type");
-            promotionTypes.add(type);
-        }
+        promotionTypes = jdbcTemplate.query(sql,new Object[]{id},new RowMapper<Integer>() {
+            @Override
+            public Integer mapRow(ResultSet resultSet, int i) throws SQLException {
+                return resultSet.getInt("type");
+            }
+        });
 
         return promotionTypes;
     }
