000fa1dd597d65a2dea2a6d29858a7180c0c27f7
SnakerWorker <snakerworker@gmail.com>
2014-11-08 17:37:42 +0800

    add promotion fields

--- src/main/java/com/thoughtworks/iamcoach/pos/model/Promotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/model/Promotion.java
@@ -2,6 +2,34 @@ package com.thoughtworks.iamcoach.pos.model;
 
 public abstract class Promotion {
 
+    private int id ;
+    private int type;
+    private String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     public abstract double getMoney(CartItem cartItem);
 
 }
