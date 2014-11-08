78cb40feaec903e738feebcefe48a606a2ccf2e4
SnakerWorker <snakerworker@gmail.com>
2014-11-08 17:22:08 +0800

    modify get method

--- src/main/java/com/thoughtworks/iamcoach/pos/utils/FileUtils.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/utils/FileUtils.java
@@ -1,15 +1,22 @@
 package com.thoughtworks.iamcoach.pos.utils;
 
 import java.io.*;
-import java.nio.file.*;
 import java.util.*;
 
 public class FileUtils {
 
     public static List<String> get (String filename) throws IOException {
 
-        Path path = Paths.get("src/main/resources",filename);
+        List<String> barcodeList = new ArrayList<String>();
+        InputStream inputStream = FileUtils.class.getClassLoader().getResourceAsStream(filename);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String barcode;
 
-        return Files.readAllLines(path);
+        while ((barcode = reader.readLine()) != null){
+            barcodeList.add(barcode);
+        }
+
+        reader.close();
+        return barcodeList;
     }
 }
